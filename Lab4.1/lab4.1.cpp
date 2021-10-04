6d227885848fef2f1b44fd158e50625520be3f05
Daniil Verkhutin <daniil.verkhutin.it.2021@lpnu.ua>
2021-10-04 10:24:19 +0300

    commit2

--- Lab4.1/lab4.1.cpp
+++ Lab4.1/lab4.1.cpp
@@ -13,7 +13,7 @@ int main() {
 	S = 0;
 	i = 1;
 	while (i <= N) {
-		S += 1/i + sqrt(1 + sin(1. * i)* sin(1. * i));
+		S += 1/(1. * i) + sqrt(1 + sin(1. * i)* sin(1. * i));
 		i++;
 	}
 	cout << "1) S = " << S << endl;
@@ -22,7 +22,7 @@ int main() {
 	S = 0;
 	i = 1;
 	do{
-		S += 1 / i + sqrt(1 + sin(1. * i) * sin(1. * i));
+		S += 1 / (1. * i) + sqrt(1 + sin(1. * i) * sin(1. * i));
 		i++;
 	} while (i <= N);
 	cout << "2) S = " << S << endl;
@@ -30,14 +30,14 @@ int main() {
 
 	S = 0;
 	for (i = 1; i <= N; i++) {
-		S += 1 / i + sqrt(1 + sin(1.*i) * sin(1. * i));
+		S += 1 / (1. * i) + sqrt(1 + sin(1. * i) * sin(1. * i));
 	}
 	cout << "3) S = " << S << endl;
 	cout << endl;
 
 	S = 0;
 	for (i = N; i >= 1; i--) {
-		S += 1 / i + sqrt(1 + sin(1. * i) * sin(1. * i));
+		S += 1 / (1. * i) + sqrt(1 + sin(1. * i) * sin(1. * i));
 	}
 	cout << "4) S = " << S << endl;
 	cout << endl;
